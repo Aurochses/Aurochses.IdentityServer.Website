1c6013e54cc0e91b498ac0f66429af884b3a4e49
Vladimir Rodchenko <vladimir@aurochses.com>
2017-12-03 19:12:09 +0300

    update db setup

--- database/Aurochses.IdentityServer.Database/setup.cmd
+++ database/Aurochses.IdentityServer.Database/setup.cmd
@@ -1,6 +1,6 @@
-dotnet ef database update -c:BaseContext %*
+dotnet ef database update -c:BaseContext
 
-dotnet ef database update -c:IdentityContext %*
+dotnet ef database update -c:IdentityContext
 
-dotnet ef database update -c:IdentityServerConfigurationContext %*
-dotnet ef database update -c:IdentityServerPersistedGrantContext %*
+dotnet ef database update -c:IdentityServerConfigurationContext
+dotnet ef database update -c:IdentityServerPersistedGrantContext
