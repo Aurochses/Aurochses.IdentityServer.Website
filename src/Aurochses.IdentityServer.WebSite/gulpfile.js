9e86b10f47f1d727a45994ca99ea158e7bf67bfd
Vladimir Rodchenko <vladimir@aurochses.com>
2018-01-31 14:17:45 +0300

    move bootstrap to v4.0 update packages to latest version

--- src/Aurochses.IdentityServer.WebSite/gulpfile.js
+++ src/Aurochses.IdentityServer.WebSite/gulpfile.js
@@ -17,7 +17,6 @@ var app = {
         "./Content/**/*.scss"
     ],
     fonts: [
-        "./node_modules/bootstrap-sass/assets/fonts/bootstrap/*.*",
         "./node_modules/font-awesome/fonts/*.*"
     ],
     images: [
@@ -25,7 +24,7 @@ var app = {
     ],
     js: [
         { src: "./Scripts/**/*.js", dest: "./wwwroot/js" },
-        { src: "./node_modules/bootstrap-sass/assets/javascripts/bootstrap.js", dest: "./wwwroot/js/bootstrap" },
+        { src: "./node_modules/bootstrap/dist/js/bootstrap.js", dest: "./wwwroot/js/bootstrap" },
         { src: "./node_modules/jquery/dist/jquery.js", dest: "./wwwroot/js/jquery" },
         { src: "./node_modules/jquery-validation/dist/jquery.validate.js", dest: "./wwwroot/js/jquery-validation" },
         { src: "./node_modules/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js", dest: "./wwwroot/js/jquery-validation-unobtrusive" }
@@ -33,44 +32,54 @@ var app = {
 };
 
 // Favicon
-gulp.task("copy-favicon", function () {
+gulp.task("copy-favicon", function (done) {
     _.forEach(app.favicon, function (file) {
         gulp.src(file)
             .pipe(gulp.dest("./wwwroot"));
     });
+
+    done();
 });
 
 // Css
-gulp.task("copy-css", function () {
+gulp.task("copy-css", function (done) {
     _.forEach(app.css, function (file) {
         gulp.src(file)
             .pipe(sass())
             .pipe(gulp.dest("./wwwroot/css"));
     });
+
+    done();
 });
 
 // Fonts
-gulp.task("copy-fonts", function () {
+gulp.task("copy-fonts", function (done) {
     _.forEach(app.fonts, function (file) {
         gulp.src(file)
             .pipe(gulp.dest("./wwwroot/fonts"));
     });
+
+    done();
 });
 
 // Images
-gulp.task("copy-images", function () {
+gulp.task("copy-images", function (done) {
     _.forEach(app.images, function (file) {
         gulp.src(file)
             .pipe(gulp.dest("./wwwroot/images"));
     });
+
+    done();
 });
 
 // JavaScript
-gulp.task("copy-js", function () {
+gulp.task("copy-js", function (done) {
     _.forEach(app.js, function (file) {
         gulp.src(file.src)
             .pipe(gulp.dest(file.dest));
     });
+
+    done();
 });
 
-gulp.task("default", ["copy-favicon", "copy-css", "copy-fonts", "copy-images", "copy-js"]);
+gulp.task("default", gulp.series("copy-favicon", "copy-css", "copy-fonts", "copy-images", "copy-js"));
