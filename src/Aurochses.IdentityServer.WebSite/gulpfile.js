27a9eaa78ddf6968bc2cd2a21cc10e3a273b8521
Vladimir Rodchenko <vladimir@aurochses.com>
2018-01-31 18:44:54 +0300

    forms UI

--- src/Aurochses.IdentityServer.WebSite/gulpfile.js
+++ src/Aurochses.IdentityServer.WebSite/gulpfile.js
@@ -6,6 +6,8 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 
 var _ = require("lodash"),
     gulp = require("gulp"),
+    gulpif = require("gulp-if"),
+    rename = require("gulp-rename"),
     sass = require("gulp-sass");
 
 // APP
@@ -23,11 +25,13 @@ var app = {
         "./Images/**"
     ],
     js: [
-        { src: "./Scripts/**/*.js", dest: "./wwwroot/js" },
+        { src: "./bower_components/jquery-validation-unobtrusive-bootstrap/dist/unobtrusive-bootstrap.js", dest: "./wwwroot/js/jquery-validation-unobtrusive-bootstrap", filename: "jquery.validate.unobtrusive.bootstrap.js" },
         { src: "./node_modules/bootstrap/dist/js/bootstrap.js", dest: "./wwwroot/js/bootstrap" },
         { src: "./node_modules/jquery/dist/jquery.js", dest: "./wwwroot/js/jquery" },
         { src: "./node_modules/jquery-validation/dist/jquery.validate.js", dest: "./wwwroot/js/jquery-validation" },
-        { src: "./node_modules/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js", dest: "./wwwroot/js/jquery-validation-unobtrusive" }
+        { src: "./node_modules/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js", dest: "./wwwroot/js/jquery-validation-unobtrusive" },
+        { src: "./node_modules/popper.js/dist/umd/popper.js", dest: "./wwwroot/js/popper.js" },
+        { src: "./Scripts/**/*.js", dest: "./wwwroot/js" }
     ]
 };
 
@@ -76,6 +80,7 @@ gulp.task("copy-images", function (done) {
 gulp.task("copy-js", function (done) {
     _.forEach(app.js, function (file) {
         gulp.src(file.src)
+            .pipe(gulpif(file.filename != null, rename(file.filename)))
             .pipe(gulp.dest(file.dest));
     });
 
