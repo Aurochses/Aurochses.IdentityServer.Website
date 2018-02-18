b228a32f5ead299b933c91a6b02b23f82392b4ca
Vladimir Rodchenko <vladimir@aurochses.com>
2018-02-18 22:16:27 +0300

    initial code for console client

---
+++ clients/Aurochses.IdentityServer.Client.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Aurochses.IdentityServer.Client.Console
+{
+    /// <summary>
+    /// Class Program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
