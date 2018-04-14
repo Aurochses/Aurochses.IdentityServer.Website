01ce377cec5c2bbfaa6f069e633ee7dda7d58317
Vladimir Rodchenko <vladimir@aurochses.com>
2018-04-14 11:27:34 +0300

    update test data

--- database/Aurochses.IdentityServer.Database.Data/Service.cs
+++ database/Aurochses.IdentityServer.Database.Data/Service.cs
@@ -1,18 +1,24 @@
-﻿using Aurochses.IdentityServer.Database.Data.IdentityServer;
+﻿using Aurochses.IdentityServer.Database.Data.Identity;
+using Aurochses.IdentityServer.Database.Data.IdentityServer;
 
 namespace Aurochses.IdentityServer.Database.Data
 {
     public class Service
     {
+        private readonly IdentityService _identityService;
         private readonly IdentityServerService _identityServerService;
 
-        public Service(IdentityServerService identityServerService)
+        public Service(IdentityService identityService, IdentityServerService identityServerService)
         {
+            _identityService = identityService;
             _identityServerService = identityServerService;
         }
 
         public void Run(string environmentName)
         {
+            // Identity
+            _identityService.Run(environmentName);
+
             // IdentityServer
             _identityServerService.Run(environmentName);
         }
