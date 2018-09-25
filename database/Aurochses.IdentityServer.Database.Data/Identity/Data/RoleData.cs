c6be06223036c6fbfc45e63fd094f1087aa9b8b8
Vladimir Rodchenko <vladimir@rodchenko.by>
2018-09-25 19:28:09 +0300

    update Data

---
+++ database/Aurochses.IdentityServer.Database.Data/Identity/Data/RoleData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Aurochses.IdentityServer.Database.Data.Identity.Models;
+
+namespace Aurochses.IdentityServer.Database.Data.Identity.Data
+{
+    public static class RoleData
+    {
+        public static IEnumerable<RoleModel> GetList(string environmentName)
+        {
+            var configuration = Program.BuildConfiguration(@"Identity\Data", nameof(RoleData), environmentName);
+
+            return configuration.GetSection("Data").Get<IList<RoleModel>>();
+        }
+    }
+}
--- database/Aurochses.IdentityServer.Database.Data/Identity/Models/UserModel.cs
+++ database/Aurochses.IdentityServer.Database.Data/Identity/Models/UserModel.cs
@@ -1,10 +1,14 @@
 ﻿using Aurochses.AspNetCore.Identity.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace Aurochses.IdentityServer.Database.Data.Identity.Models
 {
     public class UserModel
     {
         public ApplicationUser User { get; set; }
+
         public string Password { get; set; }
+
+        public IList<string> Roles { get; set; }
     }
 }
