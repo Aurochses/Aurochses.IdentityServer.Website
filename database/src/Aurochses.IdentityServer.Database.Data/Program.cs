bdbcd538aa64e3dd8d39e7a710cc256f145ac0ea
Vladimir Rodchenko <vladimir@rodchenko.by>
2019-04-10 16:26:42 +0300

    add database project

---
+++ database/src/Aurochses.IdentityServer.Database.Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aurochses.IdentityServer.Database.Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
