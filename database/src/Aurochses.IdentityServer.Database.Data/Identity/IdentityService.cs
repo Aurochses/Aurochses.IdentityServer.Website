bdd8ac887ffbb713f6d129944708c54d69fa8e02
Vladimir Rodchenko <vladimir@rodchenko.by>
2019-04-28 18:21:03 +0300

    add roles update users

--- database/src/Aurochses.IdentityServer.Database.Data/Identity/IdentityService.cs
+++ database/src/Aurochses.IdentityServer.Database.Data/Identity/IdentityService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Security.Claims;
 using Aurochses.AspNetCore.Identity.EntityFrameworkCore;
 using Aurochses.IdentityServer.Database.Data.Identity.Data;
 using AutoMapper;
@@ -10,45 +11,59 @@ namespace Aurochses.IdentityServer.Database.Data.Identity
     public class IdentityService
     {
         private readonly IMapper _mapper;
+        private readonly RoleManager<ApplicationRole> _roleManager;
         private readonly UserManager<ApplicationUser> _userManager;
 
-        public IdentityService(IMapper mapper, UserManager<ApplicationUser> userManager)
+        public IdentityService(IMapper mapper, RoleManager<ApplicationRole> roleManager, UserManager<ApplicationUser> userManager)
         {
             _mapper = mapper;
+            _roleManager = roleManager;
             _userManager = userManager;
         }
 
-        // todo: roles must be solved
         public void Run(string environmentName)
         {
+            // Role
+            foreach (var item in _roleManager.Roles.ToList())
+            {
+                var deleteIdentityResult = _roleManager.DeleteAsync(item).Result;
+
+                if (!deleteIdentityResult.Succeeded) throw new Exception($"Can't delete role: {item.Name}. Description '{deleteIdentityResult.Errors.Select(x => x.Description).FirstOrDefault()}'");
+            }
+            
+            foreach (var item in RoleData.GetList(environmentName))
+            {
+                var createIdentityResult = _roleManager.CreateAsync(item.Role).Result;
+
+                if (!createIdentityResult.Succeeded) throw new Exception($"Can't create user: {item.Role.Name}. Description '{createIdentityResult.Errors.Select(x => x.Description).FirstOrDefault()}'");
+
+                foreach (var roleClaim in item.RoleClaims)
+                {
+                    var addClaimIdentityResult = _roleManager.AddClaimAsync(item.Role, new Claim(roleClaim.ClaimType, roleClaim.ClaimValue)).Result;
+
+                    if (!addClaimIdentityResult.Succeeded) throw new Exception($"Can't add claim to role: {item.Role.Name}. Description '{addClaimIdentityResult.Errors.Select(x => x.Description).FirstOrDefault()}'");
+                }
+            }
+
             // User
+            foreach (var item in _userManager.Users.ToList())
+            {
+                var deleteIdentityResult = _userManager.DeleteAsync(item).Result;
+
+                if (!deleteIdentityResult.Succeeded) throw new Exception($"Can't delete user: {item.Email}. Description '{deleteIdentityResult.Errors.Select(x => x.Description).FirstOrDefault()}'");
+            }
+
             foreach (var item in UserData.GetList(environmentName))
             {
-                var existingItem = _userManager.FindByNameAsync(item.User.Email).Result;
+                var createIdentityResult = _userManager.CreateAsync(item.User, item.Password).Result;
 
-                if (existingItem != null)
+                if (!createIdentityResult.Succeeded) throw new Exception($"Can't create user: {item.User.Email}. Description '{createIdentityResult.Errors.Select(x => x.Description).FirstOrDefault()}'");
+
+                if (item.Roles != null)
                 {
-                    existingItem = _mapper.Map(item.User, existingItem);
+                    var addToRolesIdentityResult = _userManager.AddToRolesAsync(item.User, item.Roles).Result;
 
-                    var updateIdentityResult = _userManager.UpdateAsync(existingItem).Result;
-                    if (!updateIdentityResult.Succeeded) throw new Exception($"Can't update user: {item.User.Email}. Description '{updateIdentityResult.Errors.Select(x => x.Description).FirstOrDefault()}'");
-
-                    existingItem = _userManager.FindByNameAsync(item.User.Email).Result;
-                    if (existingItem == null) continue;
-
-                    if (!_userManager.CheckPasswordAsync(existingItem, item.Password).Result)
-                    {
-                        var token = _userManager.GeneratePasswordResetTokenAsync(existingItem).Result;
-                        var resetPasswordIdentityResult = _userManager.ResetPasswordAsync(existingItem, token, item.Password).Result;
-
-                        if (!resetPasswordIdentityResult.Succeeded) throw new Exception($"Can't reset password for user: {existingItem.Email}. Description '{resetPasswordIdentityResult.Errors.Select(x => x.Description).FirstOrDefault()}'");
-                    }
-                }
-                else
-                {
-                    var createIdentityResult = _userManager.CreateAsync(item.User, item.Password).Result;
-
-                    if (!createIdentityResult.Succeeded) throw new Exception($"Can't create user: {item.User.Email}. Description '{createIdentityResult.Errors.Select(x => x.Description).FirstOrDefault()}'");
+                    if (!addToRolesIdentityResult.Succeeded) throw new Exception($"Can't add user: {item.User.Email} to roles. Description '{addToRolesIdentityResult.Errors.Select(x => x.Description).FirstOrDefault()}'");
                 }
             }
         }
