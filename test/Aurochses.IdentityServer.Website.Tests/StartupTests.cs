6d6e3d0aaae1f23b2881f3540bb2c25ec6640220
Vladimir Rodchenko <vladimir@rodchenko.by>
2019-06-16 18:46:31 +0300

    update NuGet packages add first unit test

---
+++ test/Aurochses.IdentityServer.Website.Tests/StartupTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace Aurochses.IdentityServer.Website.Tests
+{
+    public class StartupTests
+    {
+        [Fact]
+        public void Configuration_Get_Success()
+        {
+            // Arrange
+            var configuration = new ConfigurationRoot(new List<IConfigurationProvider>());
+
+            var startup = new Startup(configuration);
+
+            // Act & Assert
+            Assert.Equal(configuration, startup.Configuration);
+        }
+    }
+}
