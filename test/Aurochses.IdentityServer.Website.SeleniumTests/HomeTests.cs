8eb4fab962ee273b092841a89aabf6bec55ef73c
Vladimir Rodchenko <vladimir@rodchenko.by>
2019-06-17 15:39:44 +0300

    add simple selenium test

---
+++ test/Aurochses.IdentityServer.Website.SeleniumTests/HomeTests.cs
@@ -0,0 +1,32 @@
+﻿using Aurochses.Xunit.Selenium;
+using OpenQA.Selenium;
+using Xunit;
+
+namespace Aurochses.IdentityServer.Website.SeleniumTests
+{
+    public class HomeTests : IClassFixture<SeleniumFixture>
+    {
+        private readonly SeleniumFixture _fixture;
+
+        public HomeTests(SeleniumFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Theory]
+        [ClassData(typeof(SeleniumWebDriverDataGenerator))]
+        public void Should_find_search_box(SeleniumWebDriverType type)
+        {
+            // Arrange
+            var driver = _fixture.GetWebDriver(type);
+
+            // Act
+            driver.Navigate().GoToUrl(_fixture.GetUrl());
+
+            // Assert
+            var p = driver.FindElement(By.TagName("p"));
+
+            Assert.Equal("p", p.TagName);
+        }
+    }
+}
