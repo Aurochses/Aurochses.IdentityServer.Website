5c7a6f27158ee0f1b405a239503393d768f3c33d
Vladimir Rodchenko <vladimir@rodchenko.by>
2019-06-17 11:35:56 +0300

    add HomeController

--- src/Aurochses.IdentityServer.Website/Controllers/HomeController.cs
+++ src/Aurochses.IdentityServer.Website/Controllers/HomeController.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Aurochses.IdentityServer.Website.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ILogger _logger;
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public HomeController(ILogger<HomeController> logger, IHostingEnvironment hostingEnvironment)
         {
-            return View();
+            _logger = logger;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index(string returnUrl = null)
+        {
+            if (_hostingEnvironment.IsDevelopment())
+            {
+                // only show in development
+                return View();
+            }
+
+            _logger.LogInformation("Homepage is disabled in production. Redirect to SignIn page.");
+            return RedirectToAction("Index", "SignIn", new { ReturnUrl = returnUrl });
         }
     }
 }
