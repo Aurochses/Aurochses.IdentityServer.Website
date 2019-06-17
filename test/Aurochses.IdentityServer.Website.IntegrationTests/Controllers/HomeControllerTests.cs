27e2bad9510b6215f59992375f2551a4fff8139b
Vladimir Rodchenko <vladimir@rodchenko.by>
2019-06-17 12:08:58 +0300

    add  simple integration test

---
+++ test/Aurochses.IdentityServer.Website.IntegrationTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Aurochses.IdentityServer.Website.IntegrationTests.Controllers
+{
+    public class HomeControllerTests : IClassFixture<WebApplicationFactory<Website.Startup>>
+    {
+        private readonly WebApplicationFactory<Website.Startup> _factory;
+
+        public HomeControllerTests(WebApplicationFactory<Website.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Index()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/");
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
