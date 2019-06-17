cbf14e0a7ca21952c1317fe3a1f4996e9fb6802d
Vladimir Rodchenko <vladimir@rodchenko.by>
2019-06-17 21:09:42 +0300

    add database integration test

---
+++ database/test/Aurochses.IdentityServer.Database.IntegrationTests/StartupTests.cs
@@ -0,0 +1,31 @@
+﻿using Aurochses.AspNetCore.Identity.EntityFrameworkCore;
+using Aurochses.IdentityServer.Database.Context;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Aurochses.IdentityServer.Database.IntegrationTests
+{
+    public class StartupTests : IClassFixture<TestServerFixture>
+    {
+        private readonly TestServerFixture _fixture;
+
+        public StartupTests(TestServerFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void ConfigureServices_Success()
+        {
+            // Arrange & Act & Assert
+            Assert.NotNull(_fixture.Server.Host.Services.GetService<BaseContext>());
+
+            Assert.NotNull(_fixture.Server.Host.Services.GetService<IdentityContext>());
+            Assert.NotNull(_fixture.Server.Host.Services.GetService<IdentityDbContext>());
+            Assert.NotNull(_fixture.Server.Host.Services.GetService<IdentityServerConfigurationContext>());
+            //Assert.NotNull(_fixture.Server.Host.Services.GetService<ConfigurationDbContext>());
+            Assert.NotNull(_fixture.Server.Host.Services.GetService<IdentityServerPersistedGrantContext>());
+            //Assert.NotNull(_fixture.Server.Host.Services.GetService<PersistedGrantDbContext>());
+        }
+    }
+}
---
+++ database/test/Aurochses.IdentityServer.Database.IntegrationTests/TestServerFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Aurochses.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+
+namespace Aurochses.IdentityServer.Database.IntegrationTests
+{
+    public class TestServerFixture : IDisposable
+    {
+        public TestServerFixture()
+        {
+            var webHostBuilder = new WebHostBuilder()
+                .UseContentRoot(ProjectHelpers.GetProjectPath(@"src", typeof(Database.Startup).Assembly))
+                .UseStartup<Startup>();
+
+            Server = new TestServer(webHostBuilder);
+        }
+
+        public TestServer Server { get; }
+
+        public void Dispose()
+        {
+            Server.Dispose();
+        }
+    }
+}
