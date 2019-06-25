991bcddcb3cf0e4f27cf92aa86bc3d5b0e8f8ed4
Vladimir Rodchenko <vladimir@rodchenko.by>
2019-06-25 22:04:15 +0300

    add Discovery Endpoint test

--- test/Aurochses.IdentityServer.Website.IntegrationTests/IdentityServerEndpointTests.cs
+++ test/Aurochses.IdentityServer.Website.IntegrationTests/IdentityServerEndpointTests.cs
@@ -1,8 +1,35 @@
-﻿namespace Aurochses.IdentityServer.Website.IntegrationTests
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Aurochses.IdentityServer.Website.IntegrationTests
 {
-    public class IdentityServerEndpointTests
+    // todo: http://docs.identityserver.io/en/latest/endpoints/discovery.html
+    public class IdentityServerEndpointTests : IClassFixture<TestWebApplicationFactoryWithDatabase>
     {
-        // todo: add tests
-        // http://docs.identityserver.io/en/latest/endpoints/discovery.html
+        private readonly HttpClient _client;
+
+        public IdentityServerEndpointTests(TestWebApplicationFactoryWithDatabase factory)
+        {
+            _client = factory.CreateClient(
+                new WebApplicationFactoryClientOptions
+                {
+                    AllowAutoRedirect = false
+                }
+            );
+        }
+
+        [Fact]
+        public async Task DiscoveryEndpoint_Success()
+        {
+            // Arrange & Act
+            var response = await _client.GetAsync("/.well-known/openid-configuration");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("application/json; charset=UTF-8", response.Content.Headers.ContentType.ToString());
+        }
     }
 }
