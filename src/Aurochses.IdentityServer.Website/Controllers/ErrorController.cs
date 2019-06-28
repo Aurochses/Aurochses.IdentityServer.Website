ebef209620905611df783baeb4ef28aba0cac56b
Vladimir Rodchenko <vladimir@rodchenko.by>
2019-06-28 19:23:08 +0300

    add ErrorController with tests

---
+++ src/Aurochses.IdentityServer.Website/Controllers/ErrorController.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using Aurochses.IdentityServer.Website.Filters;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Aurochses.IdentityServer.Website.Controllers
+{
+    [SecurityHeaders]
+    [AllowAnonymous]
+    public class ErrorController : Controller
+    {
+        private readonly ILogger _logger;
+        private readonly IHostingEnvironment _hostingEnvironment;
+        private readonly IIdentityServerInteractionService _identityServerInteractionService;
+
+        public ErrorController(ILogger<ErrorController> logger, IHostingEnvironment hostingEnvironment, IIdentityServerInteractionService identityServerInteractionService)
+        {
+            _logger = logger;
+            _hostingEnvironment = hostingEnvironment;
+            _identityServerInteractionService = identityServerInteractionService;
+        }
+
+        public async Task<IActionResult> Index(string errorId)
+        {
+            var errorMessage = await _identityServerInteractionService.GetErrorContextAsync(errorId);
+            if (errorMessage != null)
+            {
+                _logger.LogError("IdentityServer interaction error. Id: {@errorId}, Message: {@errorMessage}", errorId, errorMessage);
+
+                if (!_hostingEnvironment.IsDevelopment())
+                {
+                    errorMessage.ErrorDescription = null;
+                }
+            }
+
+            return View("Error", errorMessage);
+        }
+    }
+}
---
+++ test/Aurochses.IdentityServer.Website.Tests/Controllers/ErrorControllerTests.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Threading.Tasks;
+using Aurochses.IdentityServer.Website.Controllers;
+using Aurochses.IdentityServer.Website.Filters;
+using Aurochses.Xunit;
+using Aurochses.Xunit.AspNetCore.Mvc;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using Xunit;
+
+namespace Aurochses.IdentityServer.Website.Tests.Controllers
+{
+    public class ErrorControllerTests
+    {
+        private readonly Mock<IIdentityServerInteractionService> _mockIdentityServerInteractionService;
+
+        public ErrorControllerTests()
+        {
+            _mockIdentityServerInteractionService = new Mock<IIdentityServerInteractionService>(MockBehavior.Strict);
+        }
+
+        [Theory]
+        [InlineData(typeof(SecurityHeadersAttribute))]
+        [InlineData(typeof(AllowAnonymousAttribute))]
+        public void Attribute_Defined(Type attributeType)
+        {
+            // Arrange & Act & Assert
+            TypeAssert.HasAttribute<ErrorController>(attributeType);
+        }
+
+        [Fact]
+        public async Task Index_WhenIdentityServerInteractionServiceErrorMessageIsNull_ReturnViewResult()
+        {
+            // Arrange
+            const string errorId = "Test ErrorId";
+
+            var controller = new ErrorController(new NullLogger<ErrorController>(), new HostingEnvironment(), _mockIdentityServerInteractionService.Object);
+
+            _mockIdentityServerInteractionService
+                .Setup(x => x.GetErrorContextAsync(errorId))
+                .ReturnsAsync(() => null);
+
+            // Act
+            var actionResult = await controller.Index(errorId);
+
+            // Assert
+            MvcAssert.ViewResult(actionResult, "Error");
+        }
+
+        [Fact]
+        public async Task Index_WhenIdentityServerInteractionServiceErrorMessageIsNotNull_ReturnViewResult()
+        {
+            // Arrange
+            const string errorId = "Test ErrorId";
+
+            var controller = new ErrorController(new NullLogger<ErrorController>(), new HostingEnvironment(), _mockIdentityServerInteractionService.Object);
+
+            _mockIdentityServerInteractionService
+                .Setup(x => x.GetErrorContextAsync(errorId))
+                .ReturnsAsync(
+                    () => new ErrorMessage
+                    {
+                        DisplayMode = "Test DisplayMode",
+                        UiLocales = "Test UiLocales",
+                        Error = "Test Error",
+                        ErrorDescription = "Test ErrorDescription",
+                        RequestId = "Test RequestId",
+                        RedirectUri = "Test RedirectUri",
+                        ResponseMode = "Test ResponseMode"
+                    }
+                );
+
+            // Act
+            var actionResult = await controller.Index(errorId);
+
+            // Assert
+            MvcAssert.ViewResult(
+                actionResult,
+                "Error",
+                new ErrorMessage
+                {
+                    DisplayMode = "Test DisplayMode",
+                    UiLocales = "Test UiLocales",
+                    Error = "Test Error",
+                    ErrorDescription = null,
+                    RequestId = "Test RequestId",
+                    RedirectUri = "Test RedirectUri",
+                    ResponseMode = "Test ResponseMode"
+                }
+            );
+        }
+
+        [Fact]
+        public async Task Index_WhenIdentityServerInteractionServiceErrorMessageIsNotNullAndHostingEnvironmentIsDevelopment_ReturnViewResult()
+        {
+            // Arrange
+            const string errorId = "Test ErrorId";
+
+            var controller = new ErrorController(new NullLogger<ErrorController>(), new HostingEnvironment { EnvironmentName = "Development" }, _mockIdentityServerInteractionService.Object);
+
+            _mockIdentityServerInteractionService
+                .Setup(x => x.GetErrorContextAsync(errorId))
+                .ReturnsAsync(
+                    () => new ErrorMessage
+                    {
+                        DisplayMode = "Test DisplayMode",
+                        UiLocales = "Test UiLocales",
+                        Error = "Test Error",
+                        ErrorDescription = "Test ErrorDescription",
+                        RequestId = "Test RequestId",
+                        RedirectUri = "Test RedirectUri",
+                        ResponseMode = "Test ResponseMode"
+                    }
+                );
+
+            // Act
+            var actionResult = await controller.Index(errorId);
+
+            // Assert
+            MvcAssert.ViewResult(
+                actionResult,
+                "Error",
+                new ErrorMessage
+                {
+                    DisplayMode = "Test DisplayMode",
+                    UiLocales = "Test UiLocales",
+                    Error = "Test Error",
+                    ErrorDescription = "Test ErrorDescription",
+                    RequestId = "Test RequestId",
+                    RedirectUri = "Test RedirectUri",
+                    ResponseMode = "Test ResponseMode"
+                }
+            );
+        }
+    }
+}
